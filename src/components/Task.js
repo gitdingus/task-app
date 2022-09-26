1ff133c4809ed88121633d25fb59473c0086c490
Josh S <aaabccc87@protonmail.com>
2022-09-26 02:22:28 -0400

    Add ability to edit a task

--- src/components/Task.js
+++ src/components/Task.js
@@ -6,9 +6,11 @@ class Task extends React.Component {
 
     this.state = {
       mode: 'view',
+      task: this.props.task.task,
     };
 
     this.toggleMode = this.toggleMode.bind(this);
+    this.valueChanged = this.valueChanged.bind(this);
   }
 
   toggleMode() {
@@ -17,15 +19,48 @@ class Task extends React.Component {
     });
   }
 
+  valueChanged(e){
+    this.setState({
+      task: e.target.value,
+    })
+  }
+
   render() {
     const { task, id } = this.props.task;
-    return (
+
+    const ViewMode = (
       <div>
         {task}
         <button onClick={this.toggleMode}>Edit</button>
         <button onClick={() => this.props.removeTask(id)}>Remove</button>
       </div>
     );
+
+    const EditMode = (
+      <div>
+        <input value={this.state.task} onChange={this.valueChanged} />
+        <button onClick={() => {
+          this.props.editTask(id, this.state.task);
+          this.toggleMode();
+        }}>
+          Save
+        </button>
+        <button onClick={() => {
+          this.toggleMode();
+          this.setState({
+            task: task,
+          });
+        }}>Cancel</button>
+      </div>
+    );
+
+
+    if (this.state.mode === 'view'){
+      return ViewMode;
+    } else if (this.state.mode === 'edit') {
+      return EditMode;
+    }
+
   }
 }
 
